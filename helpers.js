089acdd7e1a9b58cbbdc279876b97aee5d7d1a64
Harcourt Hamsa <hamsaharcourt@gmail.com>
2024-08-25 00:15:10 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const environment = {
-   prod: "https://iqvalue-investments-6efaf38cc2b7.herokuapp.com/",
+   prod: "https://iq-value-backup-76f9f186fe7a.herokuapp.com/",
   dev: "http://localhost:8080",
 };
 
